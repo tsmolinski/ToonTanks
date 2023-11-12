7d1abc94730662bd7ef880d7a609768b0fd73edc
tsmolinski <tsmolinski3@gmail.com>
2023-11-12 13:05:39 +0100

    Created MovingEnemy "base" class and reparented proper BP

---
+++ Source/ToonTanks/MovingEnemyTank.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingEnemyTank.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
+
+void AMovingEnemyTank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (InFireRange())
+	{
+		RotateTurret(Tank->GetActorLocation());
+	}
+}
+
+void AMovingEnemyTank::HandleDestruction()
+{
+	Super::HandleDestruction();
+	Destroy();
+}
+
+void AMovingEnemyTank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+	GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &AMovingEnemyTank::CheckFireCondition, FireRate, true);
+}
+
+void AMovingEnemyTank::CheckFireCondition()
+{
+	if (Tank == nullptr)
+	{
+		return;
+	}
+
+	if (InFireRange() && Tank->bAlive)
+	{
+		Fire();
+	}
+}
+
+bool AMovingEnemyTank::InFireRange()
+{
+	if (Tank)
+	{
+		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+		if (Distance <= FireRange)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
